d98fd54d8e7fc34958272bde1e6c23b9b85d7651
Usman <muhammad.usmanabid571@gmail.com>
2024-07-17 20:04:47 +0500

    some fixes

--- app/components/Temperature/Temperature.tsx
+++ app/components/Temperature/Temperature.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+//import React, { useEffect, useState } from 'react'
 import { useGlobalContext } from '@/app/context/globalContext'
 import { KelvinToCelsius } from '@/app/utils/Misc';
 import moment from "moment";
@@ -12,35 +12,35 @@ function Temperature() {
         return <div>Loading...</div>;
     }
     
-    const [localTime, setLocalTime] = useState("");
-    const [currentDay, setCurrentDay] = useState("")
+    // const [localTime, setLocalTime] = useState("");
+    // const [currentDay, setCurrentDay] = useState("")
 
     const minTemp = KelvinToCelsius(main?.temp_min);
     const maxTemp = KelvinToCelsius(main?.temp_max);
     const Temp = KelvinToCelsius(main?.temp);
    
-    useEffect(() => {
+    // useEffect(() => {
         
-        const interval = setInterval(() => {
+    //     const interval = setInterval(() => {
              
             
 
-          const localMoment = moment().utcOffset(timezone *1000 );
+    //       const localMoment = moment().utcOffset(timezone *1000 );
           
-          const formatedTime = localMoment.format("HH:mm:ss:A");
+    //       const formatedTime = localMoment.format("HH:mm:ss:A");
           
-          const day = localMoment.format("dddd");
+    //       const day = localMoment.format("dddd");
     
-          setLocalTime(formatedTime);
-          setCurrentDay(day);
-        }, 1000);
-      }, []);
+    //       setLocalTime(formatedTime);
+    //       setCurrentDay(day);
+    //     }, 1000);
+    //   }, []);
     
     return (
         <div className='border p-5 rounded-lg flex flex-col justify-between dark:bg-zinc-900 shadow-sm dark:shadow-none '>
             <p className='flex justify-between items-center'>
-                <span className='font-medium'>{currentDay}</span>
-                <span className='font-medium'>{localTime}</span>
+                <span className='font-medium'></span>
+                <span className='font-medium'></span>
             </p>
             <p className='pt-2 font-bold
       flex gap-1'>
